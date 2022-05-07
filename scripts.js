de55b031a78cf2d32c2d261746856b6ef559bc68
yatpit <dearbonsh@163.com>
2022-05-07 17:49:48 +0800

    second commit

--- scripts.js
+++ scripts.js
@@ -7,26 +7,33 @@ let x_offset = 0;
 let window_width = document.documentElement.clientWidth;
 
 
-let init = () => {
-        images[0].children[0].style = 'transform: translate(0px); filter: blur(4px);';
-        images[1].children[0].style = 'transform: translate(0px); filter: blur(0px);';
-        images[2].children[0].style = 'transform: translate(0px); filter: blur(1px);';
-        images[3].children[0].style = 'transform: translate(0px); filter: blur(4px);';
-        images[4].children[0].style = 'transform: translate(0px); filter: blur(5px);';
-        images[5].children[0].style = 'transform: translate(0px); filter: blur(6px);';
-}
-
 banner.addEventListener('mouseover', (e)=>{
     x = e.clientX;
     // console.log(x);
 })
 
 banner.addEventListener('mousemove', (e)=>{
-    x_new = e.clientX;
-    x_offset = x - x_new;
-    // console.log(x_offset);
+    x_new = e.clientX;                  // 获取鼠标进入点
+    x_offset = x_new - x;               // 获取鼠标位移量  
+    for(let [index, image] of images.entries()){
+        // console.log([index, image])
+        let speed = (6 - index)*10;
+        let offset = x_offset / speed;    // 越往前的图层，移动速度越快
+        
+        // let step = x_offset / window_width;
+        let blur = Math.abs(index + (x_offset*10 / window_width));
+        // console.log(index, blur);
+        image.style.setProperty('--offset', `${offset}px`);
+        image.style.setProperty('--blur', `${blur}px`)
+    }
 })
 
 banner.addEventListener('mouseout', (e)=>{
-    init();
+    for(let [index, image] of images.entries()){
+        image.style.setProperty('--offset', `0px`);
+        image.style.setProperty('--blur', `${index}px`)
+        if(index==1) {
+            image.style.setProperty('--blur', `0px`)
+        }
+    }
 })
