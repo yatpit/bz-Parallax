94068c7db341108308b8a5e38cff8ad9de8ed2b1
yatpit <dearbonsh@163.com>
2022-05-10 21:49:12 +0800

    add module

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-const banner = document.querySelector('.banner');
-const images = document.querySelectorAll('.image');
+const banner = document.querySelector('.Banner');
+const images = document.querySelectorAll('.Banner>div');
 
 let x = 0;
 let x_new = 0;
@@ -8,12 +8,13 @@ let window_width = document.documentElement.clientWidth;
 
 
 let init = () => {
-    for(let [index, image] of images.entries()){
-        image.style.setProperty('--offset', `0px`);
-        image.style.setProperty('--blur', `${index}px`)
-        if(index==1) {
-            image.style.setProperty('--blur', `0px`)
-        }
+    for(let image of images.entries()){
+        image.style.setProperty('--Xoffset', `0px`);
+        image.style.setProperty('--Yoffset', `0px`);
+        // image.style.setProperty('--blur', `${index}px`)
+        // if(index==1) {
+        //     image.style.setProperty('--blur', `0px`)
+        // }
     }
 }
 
@@ -21,22 +22,27 @@ init();
 
 banner.addEventListener('mouseover', (e)=>{
     x = e.clientX;
+    y = e.clientY;
     // console.log(x);
 })
 
 banner.addEventListener('mousemove', (e)=>{
     x_new = e.clientX;                  // 获取鼠标进入点
-    x_offset = x_new - x;               // 获取鼠标位移量  
+    x_offset = x_new - x;               // 获取鼠标位移量
+    y_new = e.clientY;
+    y_offset = y_new - y;  
     for(let [index, image] of images.entries()){
         // console.log([index, image])
         let speed = (6 - index)*10;
-        let offset = x_offset / speed;    // 越往前的图层，移动速度越快
+        let Xoffset = x_offset / speed;    // 越往前的图层，移动速度越快
+        let Yoffset = y_offset / speed;
+        image.style.setProperty('--Xoffset', `${Xoffset}px`);
+        image.style.setProperty('--Yoffset', `${Yoffset}px`);
         
         // let step = x_offset / window_width;
-        let blur = Math.abs(index + (x_offset*10 / window_width));
+        // let blur = Math.abs(index + (x_offset*10 / window_width));
         // console.log(index, blur);
-        image.style.setProperty('--offset', `${offset}px`);
-        image.style.setProperty('--blur', `${blur}px`)
+        // image.style.setProperty('--blur', `${blur}px`)
     }
 })
 
