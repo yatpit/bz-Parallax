d86a014eb3e28cace04d3971e81fd9829e0f9fdd
yatpit <dearbonsh@163.com>
2022-05-20 08:29:35 +0800

    test

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 class Banner{
     constructor(selector) {
-        this.banner = document.querySelector('.Banner');
-        this.images = document.querySelectorAll('.Banner>div');
+        this.banner = document.querySelector(`${selector}`);
+        this.images = document.querySelectorAll(`${selector}>div`);
         this.init();
         this.bindEvent();
     }
     init() {
         // const images = this.images
         for(let image of this.images.values()){
-            console.log(image)
+            // console.log(image)
             image.style.setProperty('--Xoffset', `0px`);
             image.style.setProperty('--Yoffset', `0px`);
         }
     }
     bindEvent() {
-        let x, y, x_new, x_offset, y_new, y_offset;
+        let x, y, x_offset, y_offset;
         // let x_new = 0;
         // let x_offset = 0;
         this.banner.addEventListener('mouseover', (e)=>{
@@ -24,10 +24,9 @@ class Banner{
             // console.log(x);
         })
         this.banner.addEventListener('mousemove', (e)=>{
-            x_new = e.clientX;                  // 获取鼠标进入点
-            x_offset = x_new - x;               // 获取鼠标位移量 
-            y_new = e.clientY;
-            y_offset = y_new - y;
+            // 获取鼠标进入点
+            x_offset = e.clientX - x;               // 获取鼠标位移量 
+            y_offset = e.clientY - y;
             for(let [index, image] of this.images.entries()){
                 // console.log([index, image])
                 let speed = (6 - index)*10;
